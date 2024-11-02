50a65a41e572c2e4c53f4739213c2298bb3c9559
ArtemMahazIPZ <159789232+ArtemMahazIPZ@users.noreply.github.com>
2024-11-02 21:26:30 +0200

    Update Contact.js

---
+++ Lab04/myreactapp/src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AnimatedPage from '../components/AnimatedPage';
+
+const Contact = () => (
+    <AnimatedPage>
+        <h1>Contact Us</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolor ex, hic ipsam labore nulla possimus quia quos sint tempore ullam velit. Aut, corporis cumque deleniti deserunt distinctio earum facilis fugit id necessitatibus obcaecati porro provident quae quasi rerum sequi similique voluptas voluptatum! Dolorem doloribus hic impedit officiis perferendis, possimus.</p>
+    </AnimatedPage>
+);
+
+export default Contact;
