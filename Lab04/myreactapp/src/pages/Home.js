c50aac2b2617f975d182a34d678ae05181183575
ArtemMahazIPZ <159789232+ArtemMahazIPZ@users.noreply.github.com>
2024-11-02 21:26:49 +0200

    Update Home.js

---
+++ Lab04/myreactapp/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AnimatedPage from '../components/AnimatedPage';
+
+const Home = () => (
+    <AnimatedPage>
+        <h1>Home Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam autem deleniti ea enim, fugiat inventore numquam, provident quae repudiandae sapiente voluptatibus! Aliquid eos facilis id nobis nostrum numquam officia quaerat quia quos repellendus, sed tempore ut. Accusamus alias cumque deserunt, eum ex explicabo nam. A ad amet at commodi doloremque earum exercitationem in libero magnam quia quibusdam, quos voluptas voluptatibus! Aspernatur aut corporis illo illum maiores, nostrum nulla perspiciatis placeat sint veritatis. Architecto deserunt dignissimos doloremque dolorum ea est excepturi facilis impedit labore magni nisi non numquam obcaecati omnis provident quae, quas quibusdam recusandae repellendus rerum veritatis vitae voluptatum?.</p>
+    </AnimatedPage>
+);
+
+export default Home;
