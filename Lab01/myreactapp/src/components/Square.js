a86a47531fe5319932e07006c6e83d43c0dba73c
ArtemMahazIPZ <159789232+ArtemMahazIPZ@users.noreply.github.com>
2024-11-06 21:27:24 +0200

    Create Square.js

---
+++ Lab01/myreactapp/src/components/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Square = ({ value, onClick, highlight }) => {
+    return (
+        <button className={`square ${highlight ? 'highlight' : ''}`} onClick={onClick}>
+            {value}
+        </button>
+    );
+};
+
+export default Square;
