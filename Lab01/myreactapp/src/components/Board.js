36ccb052433f74a41bbf56d892df34c1175af458
ArtemMahazIPZ <159789232+ArtemMahazIPZ@users.noreply.github.com>
2024-11-06 21:26:06 +0200

    Update Board.js

--- Lab01/myreactapp/src/components/Board.js
+++ Lab01/myreactapp/src/components/Board.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import Square from './Square';
+import WinningLine from './WinningLine';
+
+const Board = ({ squares, onClick, winningLine }) => {
+    const renderSquare = (index) => (
+        <Square
+            value={squares[index]}
+            onClick={() => onClick(index)}
+            highlight={winningLine && winningLine.includes(index)}
+        />
+    );
+
+    return (
+        <div className="board">
+            {[...Array(9)].map((_, i) => renderSquare(i))}
+            <WinningLine line={winningLine} />
+        </div>
+    );
+};
+
+export default Board;
 
