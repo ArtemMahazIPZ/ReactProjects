21294bf42a6e32109df8934be3aeab38d997a96b
ArtemMahazIPZ <159789232+ArtemMahazIPZ@users.noreply.github.com>
2024-11-16 10:03:05 +0200

    Update DateInput.js

--- Lab03/myreactapp/src/components/DateInput.js
+++ Lab03/myreactapp/src/components/DateInput.js
@@ -1,1 +1,15 @@
+import React from 'react';
+
+const DateInput = ({ id, register, error, min, ...rest }) => (
+    <input
+        id={id}
+        type="date"
+        {...register}
+        className={error ? 'input-error' : ''}
+        min={min}
+        {...rest}
+    />
+);
+
+export default DateInput;
 
