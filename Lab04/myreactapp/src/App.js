fc542856a4b72362e60cca26b7ea42a62220ed5e
ArtemMahazIPZ <159789232+ArtemMahazIPZ@users.noreply.github.com>
2024-12-01 02:18:09 +0200

    Update App.js

--- Lab04/myreactapp/src/App.js
+++ Lab04/myreactapp/src/App.js
@@ -1,32 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import NavBar from './components/NavBar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 
-function App() {
-    const location = useLocation();
-
-    return (
-        <>
-            <NavBar />
-            <AnimatePresence>
-                <Routes location={location} key={location.pathname}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-            </AnimatePresence>
-        </>
-    );
-}
-
-export default function RootApp() {
+const App = () => {
     return (
         <Router>
-            <App />
+            <AppRoutes />
         </Router>
     );
-}
+};
+
+export default App;
